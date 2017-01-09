88645149106a1da337a43acd5d4743681803e13e
DengMins <1253788859@qq.com>
2017-01-09 08:10:54 +0800

    调拨出入库单

--- src/main/java/com/wms/controller/AlloController.java
+++ src/main/java/com/wms/controller/AlloController.java
@@ -1,14 +1,17 @@
 package com.wms.controller;
 
+import com.wms.bean.Godown;
 import com.wms.bean.MakeInventory;
 import com.wms.commons.base.BaseController;
 import com.wms.commons.utils.ExcelToDisk;
+import com.wms.commons.utils.OrderNumberUtil;
+import com.wms.service.GodownService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,22 +20,72 @@ import java.util.Date;
  * 新增调拨单
  * Created by DengMin on 2017/1/6.
  */
-
 @Controller
 @RequestMapping("/allo")
 public class AlloController extends BaseController{
+
+    @Autowired
+    private GodownService godownService;
 
     @GetMapping("/allpage")
     public String allopage(){
         return "adjustment/allo";
     }
 
-    @RequestMapping("/ToDiskExcelout")
-    public void ToDiskExcelout(@RequestParam("data") Object data[], HttpServletResponse resp){
+    @RequestMapping("/veri")
+    @ResponseBody
+    public Object Verification(@RequestParam("Warehouse") String Warehouse,@RequestParam("cTotalvolume") String cTotalvolume,@RequestParam("cWhid") String cWhid){
+        Godown godown = godownService.selectByPrimaryKey(Integer.valueOf(Warehouse));
+        if(cWhid.equals(godown.getGoWhid())){
+            return renderError("该操作不能在同一仓库进行");
+        }else{
+            if(Double.valueOf(cTotalvolume)> godown.getGoRdvolume()){
+                return renderError("该仓库体积不足");
+            }
+        }
+        return renderSuccess("打印成功");
+    }
+
+    /**
+     * 调拨入库
+     * @param data
+     * @param resp
+     * @return
+     */
+    @RequestMapping("/ToDiskExcelput")
+    public void ToDiskExcelput(@RequestParam("data") String data[], HttpServletResponse resp){
         ExcelToDisk<MakeInventory> ex = new ExcelToDisk<MakeInventory>();
-        String [] title = {"ID","货物名称","货物型号","仓库编号","实际盘点数量","盘点人"};
+        ExcelToDisk<MakeInventory> ex1 = new ExcelToDisk<MakeInventory>();
+        String [] title = {"货物名称","货物型号","调拨数量","货物体积","仓库名称","调拨单号","调拨时间"};
+        String cName = data[0];
+        String cSkumodel = data[1];
+        String cNum = data[2];
+        String cTotalvolume = data[3];
+        String cWhid = data[5];
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String arr [] = {cName,cSkumodel,cNum,cTotalvolume,cWhid, OrderNumberUtil.generateOrderNo(),s.format(new Date())};
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
-        ex.Excel(data,"调拨出库单"+sdf.format(new Date())+".xls",title,resp);
-        ex.Excel(data,"调拨入库单"+sdf.format(new Date())+".xls",title,resp);
+       ex1.Excel(arr, "调拨入库单" + sdf.format(new Date()) + ".xls", title, resp);
+    }
+
+    /**
+     * 调拨出库
+     * @param data
+     * @param resp
+     */
+    @RequestMapping("/ToDiskExcelout")
+    public void ToDiskExcelout(@RequestParam("data") String data[], HttpServletResponse resp){
+        ExcelToDisk<MakeInventory> ex = new ExcelToDisk<MakeInventory>();
+        ExcelToDisk<MakeInventory> ex1 = new ExcelToDisk<MakeInventory>();
+        String [] title = {"货物名称","货物型号","调拨数量","货物体积","仓库名称","调拨单号","调拨时间"};
+        String cName = data[0];
+        String cSkumodel = data[1];
+        String cNum = data[2];
+        String cTotalvolume = data[3];
+        String Warehouse = data[4];
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String arr [] = {cName,cSkumodel,cNum,cTotalvolume,Warehouse, OrderNumberUtil.generateOrderNo(),s.format(new Date())};
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
+        ex.Excel(arr,"调拨出库单"+sdf.format(new Date())+".xls",title,resp);
     }
 }
